9becf33327093301dd9cb256334e0253b7bfd43a
Sarang1234 <83072803+Sarang1234@users.noreply.github.com>
2021-05-11 20:41:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,13 +31,34 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
+
+	boxPosition = width/2-100
+	boxY = 610;
+	
+	boxleftSprite = createSprite(boxPosition, boxY, 20, 100);
+    boxleftSprite.shapeColor = color (255,0,0);
+
+	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY,20,100, {isStatic:true});
+    World.add (world, boxLeftBody);
+
+	boxBase = createSprite(boxPosition+100, boxY+40, 200, 20);
+    boxBase.shapeColor =  "red";
+
+	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20,200,20, {isStatic:true});
+	World.add (world, boxBottomBody);
+
+	boxleftSprite= createSprite(boxPosition+200, boxY, 20,100);
+	boxleftSprite.shapeColor = "red";
+
+	boxRightBody = Bodies.rectangle(boxPosition+200-20, boxY,20,100, {isStatic:true});
+	World.add (world, boxRightBody);
 
 
 	Engine.run(engine);
